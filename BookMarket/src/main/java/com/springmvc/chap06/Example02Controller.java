ceea1539a6f392325487ed09806bb8106862802d
kidchang93 <lck930721@gmail.com>
2023-12-19 10:14:49 +0900

    add : 개인 포트폴리오 용 책 쇼핑몰 제작

---
+++ BookMarket/src/main/java/com/springmvc/chap06/Example02Controller.java
@@ -0,0 +1,26 @@
+package com.springmvc.chap06;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/home") //, method= RequestMethod.GET) // 생략 가능
+public class Example02Controller {
+
+    @GetMapping("/exam02/{category}/publisher/{publisher}")
+    public String requestMethod(@PathVariable String category ,
+                                @PathVariable String publisher ,
+                                Model model){
+
+
+        System.out.println("도서 분야 : " + category);
+        System.out.println("출판사 : " + publisher);
+        model.addAttribute("data","도서 분야 : " + category + "<br>" +
+                "출판사 : " + publisher);
+        return "webpage06";
+    }
+}
+// url : http://localhost:8092/BookMarket/spring/exam02
