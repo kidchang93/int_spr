5351a7d7dc5de6d8159c321b181a7c68c7614efc
kidchang93 <lck930721@gmail.com>
2023-12-21 17:56:12 +0900

    selfCheck end

--- member_framework_self/src/main/java/kr/co/chunjae/controller/MemberController.java
+++ member_framework_self/src/main/java/kr/co/chunjae/controller/MemberController.java
@@ -1,18 +1,77 @@
 package kr.co.chunjae.controller;
 
+import kr.co.chunjae.dto.MemberDTO;
+import kr.co.chunjae.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 @RequestMapping("/member")
 @RequiredArgsConstructor
+@Log4j2
 public class MemberController {
 
-
+  private final MemberService memberService;
   @GetMapping("/save")
   public String saveForm(){
     return "save";
+  }
+
+  @PostMapping("/save")
+  public String save(@ModelAttribute MemberDTO memberDTO){
+    int saveResult = memberService.save(memberDTO);
+    if (saveResult > 0) {
+      return "login";
+    } else {
+      return "save";
+    }
+  }
+
+  @GetMapping("/login")
+  public String loginForm(){
+    return "login";
+  }
+
+  @PostMapping("/login")
+  public String login(@ModelAttribute MemberDTO memberDTO , HttpSession session){
+    boolean loginResult = memberService.login(memberDTO);
+    if (loginResult){
+      session.setAttribute("loginEmail", memberDTO.getMemberEmail());
+      return "main";
+    } else {
+      return "login";
+    }
+  }
+
+  @GetMapping("/")
+  public String findAll(Model model){
+    List<MemberDTO> memberDTOList = memberService.findAll();
+    model.addAttribute("memberList",memberDTOList);
+    return "list";
+  }
+
+  // /member?id=1294
+  @GetMapping
+  public String findById(@RequestParam ("id") Long id , Model model){
+    log.info(id);
+    MemberDTO memberDTO = memberService.findById(id);
+    model.addAttribute("member",memberDTO);
+    return "detail";
+  }
+
+  @GetMapping("/delete")
+  public String deleteById(@RequestParam ("id") Long id){
+    int deleteResult = memberService.deleteById(id);
+    if (deleteResult > 0){
+      return "redirect:/member/";
+    } else {
+      return "redirect:index";
+    }
   }
 }
---
+++ member_framework_self/src/main/java/kr/co/chunjae/service/MemberService.java
@@ -0,0 +1,42 @@
+package kr.co.chunjae.service;
+
+import kr.co.chunjae.dto.MemberDTO;
+import kr.co.chunjae.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+  private final MemberRepository memberRepository;
+
+  public int save(MemberDTO memberDTO) {
+    return memberRepository.save(memberDTO);
+  }
+
+  public boolean login(MemberDTO memberDTO) {
+    MemberDTO loginMember = memberRepository.login(memberDTO);
+    if (loginMember != null) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+
+  public List<MemberDTO> findAll() {
+    return memberRepository.findAll();
+    
+  }
+
+  public MemberDTO findById(Long id) {
+    return memberRepository.findById(id);
+  }
+
+  public int deleteById(Long id) {
+    return memberRepository.deleteById(id);
+  }
+}
