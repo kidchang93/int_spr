80f79672fc9e371047efa1c268c7b6f83e5e67a4
kidchang93 <lck930721@gmail.com>
2023-12-21 09:53:55 +0900

    회원가입 컨트롤러에서 경로 정해줌

--- member_framework/src/main/java/kr/co/chunjae/MemberController.java
+++ member_framework/src/main/java/kr/co/chunjae/MemberController.java
@@ -1,4 +1,16 @@
 package kr.co.chunjae;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
 public class MemberController {
+
+  @GetMapping("/save")
+  public String saveForm(){
+    return "save";
+  }
+
 }
