ad055dfe9a7a10624a2aa16da46da53784dea14d
kidchang93 <lck930721@gmail.com>
2024-01-02 14:19:20 +0900

    스프링 file upload / download 기본 설정 파일

--- ex05/src/main/java/kr/co/chunjae/controller/UploadController.java
+++ ex05/src/main/java/kr/co/chunjae/controller/UploadController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+
 @Controller
 @Log4j
 public class UploadController {
@@ -19,12 +21,24 @@ public class UploadController {
   @PostMapping("uploadFormAction")
   public void uploadPost(MultipartFile[] uploadFile , Model model){
 
+    String uploadFolder = "E:\\upload\\temp";
+
     for (MultipartFile multipartFile : uploadFile){
 
       log.info("-------------------------------------");
       log.info("upload file name : " + multipartFile.getOriginalFilename());
       log.info("upload file size : " + multipartFile.getSize());
-    }
+
+      File saveFile = new File(uploadFolder , multipartFile.getOriginalFilename());
+
+      try{
+        multipartFile.transferTo(saveFile);
+      }catch(Exception e){
+        log.error(e.getMessage());
+      } // end catch
+
+    } // end for
+
 
   }
 }
