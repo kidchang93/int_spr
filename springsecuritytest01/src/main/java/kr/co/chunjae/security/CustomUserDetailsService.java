64ea5dd12dfcc5f262f55d006679fa7675050ce3
kidchang93 <lck930721@gmail.com>
2024-01-04 00:10:13 +0900

    log4j2설정 완료 / 시큐리티 권한 별 페이지 이동 완료

--- springsecuritytest01/src/main/java/kr/co/chunjae/security/CustomUserDetailsService.java
+++ springsecuritytest01/src/main/java/kr/co/chunjae/security/CustomUserDetailsService.java
@@ -1,6 +1,9 @@
 package kr.co.chunjae.security;
 
+import kr.co.chunjae.domain.CustomUser;
+import kr.co.chunjae.domain.MemberVO;
 import kr.co.chunjae.mapper.MemberMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @Log4j2
+
 public class CustomUserDetailsService implements UserDetailsService {
 
   @Autowired
@@ -17,6 +21,12 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     log.warn("load User By UserName : + " + userName);
 
-    return null;
+    // userName means userid
+    MemberVO vo = memberMapper.read(userName);
+
+    log.warn("queried by member mapper : " + vo);
+
+
+    return vo == null ? null : new CustomUser(vo);
   }
 }
