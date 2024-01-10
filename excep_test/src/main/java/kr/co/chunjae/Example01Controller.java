72efb378b853aed103cecf7fe4a3f4cd677dfdbd
kidchang93 <lck930721@gmail.com>
2024-01-10 14:23:52 +0900

    log4j 로깅 기록 저장 ( 난 log4j2여서 설정이 안맞음 )

--- excep_test/src/main/java/kr/co/chunjae/Example01Controller.java
+++ excep_test/src/main/java/kr/co/chunjae/Example01Controller.java
@@ -1,5 +1,7 @@
 package kr.co.chunjae;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,11 +11,17 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @Controller
 public class Example01Controller {
 
-  @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "이창규")
-  @GetMapping("/exam01")
-  public String requestMethod(Model model){
-    System.out.println("chapter10_01 예제입니다.");
-    model.addAttribute("data" , "@ResponseStatus 처리 예제입니다.");
-    return "webpage10_01";
-  }
+public static Logger logger = LoggerFactory.getLogger(Example01Controller.class);
+
+    @GetMapping("/exam01")
+    public String requestMethod(Model model){
+
+      logger.trace("Trace 메시지!");
+      logger.debug("Debug 메시지!");
+      logger.info("Info 메시지!");
+      logger.warn("Warn 메시지!");
+      logger.error("Error 메시지!");
+
+      return "webpage11_01";
+    }
 }
---
+++ excep_test/target/excep_test-1.0.0-BUILD-SNAPSHOT/WEB-INF/views/webpage11_01.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: user
+  Date: 2024-01-10
+  Time: 오후 1:50
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>로그 기록</title>
+</head>
+<body>
+<h1>로그 기록 예</h1>
+<p>콘솔에 로그 메시지가 출력되었습니다.</p>
+</body>
+</html>
