e547e0cfbc2e2883ec30caa05c4868ff8e7605b0
kidchang93 <lck930721@gmail.com>
2024-01-10 13:29:38 +0900

    스프링 예외처리 사용법

---
+++ excep_test/src/main/java/kr/co/chunjae/Example04Controller.java
@@ -0,0 +1,17 @@
+package kr.co.chunjae;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class Example04Controller {
+
+  @GetMapping("/exam04")
+  public void handleRequest(){
+    throw new Example03Exception();
+  }
+
+
+}
