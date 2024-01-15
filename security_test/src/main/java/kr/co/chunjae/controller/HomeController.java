8602ebdb558c0babd1525d2e83c7a62d826b4daa
kidchang93 <lck930721@gmail.com>
2024-01-15 11:16:47 +0900

    accessDinedHandler 인터페이스 사용

--- security_test/src/main/java/kr/co/chunjae/controller/HomeController.java
+++ security_test/src/main/java/kr/co/chunjae/controller/HomeController.java
@@ -1,4 +1,4 @@
-package kr.co.chunjae;
+package kr.co.chunjae.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
--- security_test/src/main/java/kr/co/chunjae/controller/SampleController.java
+++ security_test/src/main/java/kr/co/chunjae/controller/SampleController.java
@@ -1,4 +1,4 @@
-package kr.co.chunjae;
+package kr.co.chunjae.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
