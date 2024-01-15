05c0959c4c6ae173cafb60e541541f6780938551
kidchang93 <lck930721@gmail.com>
2024-01-15 10:16:31 +0900

    security sample 파일 및 각 기능 적용

---
+++ security_test/src/main/java/kr/co/chunjae/SampleController.java
@@ -0,0 +1,27 @@
+package kr.co.chunjae;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/sample")
+@Log4j2
+public class SampleController {
+  @GetMapping("/all")
+  public void doAll(){
+    log.info("do all can access everybody");
+  }
+
+  @GetMapping("/member")
+  public void doMember(){
+    log.info("logined member");
+  }
+
+  @GetMapping("/admin")
+  public void doAdmin(){
+    log.info("admin only");
+  }
+
+}
